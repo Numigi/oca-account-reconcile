ac982ad4ccf62bcdeab4e6582dce6597518d320a
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-09-03 11:39:35 +0100

    [16.0][IMP] add bank statement status TU

--- account_reconcile_oca/models/account_bank_statement.py
+++ account_reconcile_oca/models/account_bank_statement.py
@@ -1,15 +1,16 @@
 # Copyright 2024 Dixmit
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class AccountBankStatement(models.Model):
     _name = "account.bank.statement"
-    _inherit = ["account.bank.statement", 'mail.thread', 'mail.activity.mixin']
+    _inherit = ["account.bank.statement", "mail.thread", "mail.activity.mixin"]
 
-    is_reconciled = fields.Boolean(string="Approved", compute='_compute_is_reconciled',
-        store=True)
+    is_reconciled = fields.Boolean(
+        string="Approved", compute="_compute_is_reconciled", store=True
+    )
     state = fields.Selection(
         [
             ("open", "Open"),
@@ -20,21 +21,23 @@ class AccountBankStatement(models.Model):
         default="open",
         readonly=True,
         help="The status is set to Open, when a statement is created.\n"
-        "When the statement is approved , the status is set Confirmed.\n"
+        "When the statement is approved , the status is set Confirmed.\n",
     )
 
-    @api.depends('is_complete', 'line_ids.is_reconciled')
+    @api.depends("is_complete", "line_ids.is_reconciled")
     def _compute_is_reconciled(self):
         for stmt in self:
-            stmt.is_reconciled = (all(line.is_reconciled for line in stmt.line_ids)
-                               and stmt.is_complete)
+            stmt.is_reconciled = (
+                all(line.is_reconciled for line in stmt.line_ids) and stmt.is_complete
+            )
 
     def _compute_date_index(self):
         for stmt in self:
             sorted_lines = stmt.line_ids.filtered(lambda line: line._origin.id).sorted(
-                'internal_index')
+                "internal_index"
+            )
             stmt.first_line_index = sorted_lines[:1].internal_index
-            stmt.date = sorted_lines.filtered(lambda l: l.state == 'posted')[-1:].date
+            stmt.date = sorted_lines.filtered(lambda l: l.state == "posted")[-1:].date
 
     def action_open_statement(self):
         self.ensure_one()
@@ -47,14 +50,22 @@ class AccountBankStatement(models.Model):
     def action_confirm_statement(self):
         for rec in self:
             if not rec.is_valid:
-                raise ValidationError(_("Starting balance must match "
-                                        "the ending balance of the previous statement."))
+                raise ValidationError(
+                    _(
+                        "Starting balance must match "
+                        "the ending balance of the previous statement."
+                    )
+                )
             if not rec.is_reconciled:
                 raise ValidationError(_("All statement lines must be reconciled."))
 
             if not rec.is_complete:
-                raise ValidationError(_("Sum of statement lines must equal to the "
-                                        "difference between start and end balance"))
+                raise ValidationError(
+                    _(
+                        "Sum of statement lines must equal to the "
+                        "difference between start and end balance"
+                    )
+                )
 
             rec.state = "confirmed"
 
@@ -62,6 +73,3 @@ class AccountBankStatement(models.Model):
         for statement in self:
             statement.line_ids.unlink()
         return super(AccountBankStatement, self).unlink()
-
-
-
